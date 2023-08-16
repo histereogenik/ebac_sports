4af9dafcf5fda4c62b0dcb020d7d4d0e116836a9
Humberto Nacif <gigantedeluz@gmail.com>
2023-08-15 22:25:37 -0300

    atualizando a API para o Redux

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,14 +1,17 @@
+import { useSelector } from 'react-redux'
+
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
 
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-}
+import { RootReducer } from '../store'
 
-const ProdutosComponent = ({ produtos, favoritos }: Props) => {
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+  const favoritos = useSelector((state: RootReducer) => state.favoritos.itens)
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
     const IdsDosFavoritos = favoritos.map((f) => f.id)
@@ -16,10 +19,12 @@ const ProdutosComponent = ({ produtos, favoritos }: Props) => {
     return IdsDosFavoritos.includes(produtoId)
   }
 
+  if (isLoading) return <h2>Carregando...</h2>
+
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
